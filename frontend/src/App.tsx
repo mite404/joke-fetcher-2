1459e05db30997fc98c84684697f50fb55efa8d7
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2026-02-16 19:44:02 -0500

    solution

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,36 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import "./index.css";
+import { useState } from "react";
+
+interface Joke {
+  id?: string;
+  setup: string;
+  punchline: string;
+}
 
 export function App() {
+  const [joke, setJoke] = useState<Joke | null>(null);
+
+  async function fetchJoke() {
+    const response = await fetch("http://localhost:3000/joke");
+    const result = await response.json();
+    console.log("Joke received:", result);
+    setJoke(result);
+  }
+
   return (
     <>
-      <div>hello</div>
-      <button>Get a Joke</button>
+      <button className="cursor-auto" onClick={() => fetchJoke()}>
+        Get a Joke
+      </button>
+      {joke && (
+        <div>
+          <p>
+            <strong>{joke.setup}</strong>
+          </p>
+          <p>{joke.punchline}</p>
+        </div>
+      )}
     </>
   );
 }
